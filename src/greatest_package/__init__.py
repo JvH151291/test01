a784d46a8b4465aa1de3a515bb92e890f14e76fd
dheimgartner <d.heimgartners@gmail.com>
2023-01-23 21:31:37 +0100

    implemented greetings module

---
+++ src/greatest_package/__init__.py
@@ -0,0 +1,13 @@
+"""
+greatest_package
+=========
+
+This package is the greatest package ever - believe me. It is really great!
+
+:author: Donald
+"""
+
+__version__ = '0.0.1'
+
+def version():
+    return __version__
--- src/greatest_package/daniehei_greets.py
+++
@@ -1,2 +0,0 @@
-def daniehei_greets(my_name="Daniel"):
-    print("{} welcomes you to the Projektübung Verkehr class".format(my_name))
