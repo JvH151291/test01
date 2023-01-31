1846aa38ed9bf5f9b1263e857aa988131e8a382c
flfuchs <flfuchs@student.ethz.ch>
2023-01-31 18:03:15 +0100

    Add dummy unittest

---
+++ test_greatest_package/test_dummy.py
@@ -0,0 +1,20 @@
+import unittest
+from unittest import skip
+
+
+class ExampleTestCase(unittest.TestCase):
+
+    def test_something_that_is_true(self) -> None:
+        self.assertEqual(True, True)
+
+    @skip
+    def skip_me(self) -> None:
+        self.assertEqual(False, True)
+
+    def test_some_error(self) -> None:
+        with self.assertRaises(ZeroDivisionError):
+            a = 1 / 0
+
+
+if __name__ == '__main__':
+    unittest.main()
