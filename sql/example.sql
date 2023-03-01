44ef6e7baac7da165447e21bb67bc5a82155b8e4
Daniel Heimgartner <d.heimgartners@gmail.com>
2023-03-01 10:13:07 +0100

    Added hint for windows users

--- sql/example.sql
+++ sql/example.sql
@@ -7,4 +7,7 @@ CREATE TABLE daniehei(
 );
 
 -- Copy from file
-\copy daniehei( NAME, AGE ) FROM '~/gitlab/PuvIntro/sql/daniehei.csv' delimiter ';' CSV HEADER;
+-- for windows user specify the path as follows
+-- \copy daniehei( NAME, AGE ) FROM 'C:\\Users\\daniehei\\gitlab\\PuvIntro\\sql\\daniehei.csv' delimiter ';' CSV HEADER;
+-- for unix users
+\copy daniehei( NAME, AGE ) FROM '~/gitlab/PuvIntro/sql/daniehei.csv'
